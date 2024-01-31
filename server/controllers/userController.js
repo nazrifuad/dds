bdc0eef69946b1acc5fa6e8c8f16275309ae375e
Sivaa11 <intern@gravitas.my>
2024-01-31 10:44:25 +0800

    signin update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -66,3 +66,55 @@ exports.createUser = async (req, res) => {
     });
   }
 };
+
+exports.signIn = async (req, res) => {
+  try {
+    const connection = req.dbConnection;
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    connection.query("SELECT * FROM user WHERE email = ?", [email], async (err, rows) => {
+      if (err) {
+        console.error("Error executing query:", err);
+        return res.status(500).json({
+          status: "error",
+          message: "Internal Server Error",
+        });
+      }
+
+      if (rows.length === 0) {
+        return res.status(401).json({
+          status: "error",
+          message: "Invalid credentials",
+        });
+      }
+
+      const isPasswordValid = await bcrypt.compare(password, rows[0].password);
+
+      if (!isPasswordValid) {
+        return res.status(401).json({
+          status: "error",
+          message: "Invalid credentials",
+        });
+      }
+
+      res.status(200).json({
+        status: "success",
+        message: "User authenticated successfully",
+        data: {
+          user: rows[0],
+        },
+      });
+
+      connection.release();
+    });
+  } catch (err) {
+    console.error("Unexpected error:", err);
+    res.status(500).json({
+      status: "error",
+      message: "Internal Server Error",
+    });
+  }
+};
+
